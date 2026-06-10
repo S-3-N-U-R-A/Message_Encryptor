ca6f92a9d1598426baf5019d73a2a8728424169d
SENURA-404 <senurabuthmindpcsmf@gmail.com>
2026-06-10 13:23:15 +0530

    pop-up window for the application set up

---
+++ m_encryptor.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import messagebox
+import base64
+import os
+
+def main_screen():
+    
+    screen=Tk()
+    screen.geometry("375x398")
+
+    screen.title("Encryptor")
+
+    screen.mainloop()
+
+main_screen()
