f8b7e24eff6e10afc86636ac0812f403fb21e6ba
SENURA-404 <senurabuthmindpcsmf@gmail.com>
2026-06-12 21:18:34 +0530

    encode function implemented

--- m_encryptor.py
+++ m_encryptor.py
@@ -3,11 +3,36 @@ from tkinter import messagebox
 import base64
 import os
 
+def decrypt():
+    print("")
 
+def encrypt():
+    password=code.get()
+
+    if password=="1234":
+        screen1=Toplevel(screen)
+        screen1.title("encryption")
+        screen1.geometry("400x200")
+        screen1.configure(bg="#ed3833")
+
+        message=text1.get(1.0,END)
+        encode_message=message.encode("ascii")
+        base64_bytes=base64.b64encode(encode_message)
+        encrypt=base64_bytes.decode("ascii")
+
+        Label(screen1,text="Encypt",font="arial",fg="black",bg="white").place(x=10,y=0)
+        text2=Text(screen1,font="Roboto 10", bg="black",relief=GROOVE,wrap=WORD,bd=0)
+        text2.place(x=10,y=40,width=380,height=150)
+
+        text2.insert(END,encrypt)
 
 
 
 def main_screen():
+
+    global screen
+    global code
+    global text1
     
     screen=Tk()
     screen.geometry("500x398")
@@ -30,8 +55,8 @@ def main_screen():
     code=StringVar()
     Entry(textvariable=code,width=19,bd=2,font=("arial",25),show="*").place(x=10,y=200)
 
-    Button(text="Encrypt",height="2",width=23,bg="#ed3833",fg="black",bd=1).place(x=10,y=250)
-    Button(text="Decrypt",height="2",width=23,bg="#00bd56",fg="black",bd=1).place(x=240,y=250)
+    Button(text="Encrypt",height="2",width=23,bg="#ed3833",fg="black",bd=1,command=encrypt).place(x=10,y=250)
+    Button(text="Decrypt",height="2",width=23,bg="#00bd56",fg="black",bd=1,command=decrypt).place(x=240,y=250)
     Button(text="Reset",height="2",width=50,bg="#FFBF00",fg="black",bd=1,command=reset).place(x=10,y=300)
 
     
